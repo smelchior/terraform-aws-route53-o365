305d10bbeb2a37d94573d71cb1fc63c9baf6674f
Sebastian Melchior <sebastian@melchior.me>
2019-11-25 21:20:15 +0100

    fix tf formating

--- main.tf
+++ main.tf
@@ -1,57 +1,57 @@
 provider "template" {
-    version = "~> 2.0"
+  version = "~> 2.0"
 }
 
 data "template_file" "domain_guid" {
-    template = "${replace(var.domain, ".", "-")}"
+  template = replace(var.domain, ".", "-")
 }
 
 locals {
-    o365_mx   = "${format("10 %s.mail.protection.outlook.com", data.template_file.domain_guid.rendered)}"
-    o365_spf  = "v=spf1 include:spf.protection.outlook.com -all"
-    dkim_dom  = "${format("%s._domainkey.%s.onmicrosoft.com", data.template_file.domain_guid.rendered, var.tenant_name)}"
-    dkim = [
-        {
-            name  = "${format("selector1._domainkey.%s", var.domain)}"
-            value = "${format("selector1-%s", local.dkim_dom)}"
-        },
-        {
-            name  = "${format("selector2._domainkey.%s", var.domain)}"
-            value = "${format("selector2-%s", local.dkim_dom)}"
-        },
-    ]
-    sfb = [
-        {
-            name   = "lyncdiscover"
-            record = "webdir.online.lync.com"
-            type   = "CNAME"
-        },
-        {
-            name   = "sip"
-            record = "sipdir.online.lync.com"
-            type   = "CNAME"
-        },
-        {
-            name   = "_sipfederationtls._tcp"
-            record = "100 1 5061 sipfed.online.lync.com"
-            type   = "SRV"
-        },
-        {
-            name   = "_sip._tls"
-            record = "100 1 443 sipdir.online.lync.com"
-            type   = "SRV"
-        }
-    ]
-    mdm = [
-        {
-            name   = "enterpriseregistration"
-            record = "enterpriseregistration.windows.net"
-        },
-        {
-            name   = "enterpriseenrollment"
-            record = "enterpriseenrollment.manage.microsoft.com"
-        }
-    ]
+  o365_mx  = format("10 %s.mail.protection.outlook.com", data.template_file.domain_guid.rendered)
+  o365_spf = "v=spf1 include:spf.protection.outlook.com -all"
+  dkim_dom = format("%s._domainkey.%s.onmicrosoft.com", data.template_file.domain_guid.rendered, var.tenant_name)
+  dkim = [
+    {
+      name  = format("selector1._domainkey.%s", var.domain)
+      value = format("selector1-%s", local.dkim_dom)
+    },
+    {
+      name  = format("selector2._domainkey.%s", var.domain)
+      value = format("selector2-%s", local.dkim_dom)
+    },
+  ]
+  sfb = [
+    {
+      name   = "lyncdiscover"
+      record = "webdir.online.lync.com"
+      type   = "CNAME"
+    },
+    {
+      name   = "sip"
+      record = "sipdir.online.lync.com"
+      type   = "CNAME"
+    },
+    {
+      name   = "_sipfederationtls._tcp"
+      record = "100 1 5061 sipfed.online.lync.com"
+      type   = "SRV"
+    },
+    {
+      name   = "_sip._tls"
+      record = "100 1 443 sipdir.online.lync.com"
+      type   = "SRV"
+    }
+  ]
+  mdm = [
+    {
+      name   = "enterpriseregistration"
+      record = "enterpriseregistration.windows.net"
+    },
+    {
+      name   = "enterpriseenrollment"
+      record = "enterpriseenrollment.manage.microsoft.com"
+    }
+  ]
 }
 
 #################
@@ -59,63 +59,63 @@ locals {
 #################
 
 resource "aws_route53_record" "mx" {
-    count   = "${var.enable_exchange && ! var.enable_custom_mx ? 1 : 0}"
+  count = var.enable_exchange && ! var.enable_custom_mx ? 1 : 0
 
-    zone_id = var.zone_id
-    name    = ""
-    records = [local.o365_mx]
-    type    = "MX"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = ""
+  records = [local.o365_mx]
+  type    = "MX"
+  ttl     = var.ttl
 }
 
 resource "aws_route53_record" "custom_mx" {
-    count   = "${var.enable_exchange && var.enable_custom_mx && length(var.custom_mx_record) > 0 ? 1 : 0}"
+  count = var.enable_exchange && var.enable_custom_mx && length(var.custom_mx_record) > 0 ? 1 : 0
 
-    zone_id = var.zone_id
-    name    = ""
-    records = [var.custom_mx_record]
-    type    = "MX"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = ""
+  records = [var.custom_mx_record]
+  type    = "MX"
+  ttl     = var.ttl
 }
 
 resource "aws_route53_record" "autodiscover" {
-    count   = "${var.enable_exchange ? 1 : 0}"
+  count = var.enable_exchange ? 1 : 0
 
-    zone_id = var.zone_id
-    name    = "autodiscover"
-    records = ["autodiscover.outlook.com"]
-    type    = "CNAME"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = "autodiscover"
+  records = ["autodiscover.outlook.com"]
+  type    = "CNAME"
+  ttl     = var.ttl
 }
 
 resource "aws_route53_record" "spf" {
-    count   = "${var.enable_exchange && length(var.ms_txt) > 0 ? 1 : 0}"
+  count = "${var.enable_exchange && length(var.ms_txt) > 0 ? 1 : 0}"
 
-    zone_id = var.zone_id
-    name    = ""
-    records = flatten(["MS=${var.ms_txt}", local.o365_spf, var.additional_txt_records])
-    type    = "TXT"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = ""
+  records = flatten(["MS=${var.ms_txt}", local.o365_spf, var.additional_txt_records])
+  type    = "TXT"
+  ttl     = var.ttl
 }
 
 resource "aws_route53_record" "dmarc" {
-    count   = "${var.enable_exchange && var.enable_dmarc && length(var.dmarc_record) > 0 ? 1 : 0}"
+  count = var.enable_exchange && var.enable_dmarc && length(var.dmarc_record) > 0 ? 1 : 0
 
-    zone_id = var.zone_id
-    name    = "_dmarc"
-    records = [var.dmarc_record]
-    type    = "TXT"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = "_dmarc"
+  records = [var.dmarc_record]
+  type    = "TXT"
+  ttl     = var.ttl
 }
 
 resource "aws_route53_record" "dkim" {
-    count   = "${var.enable_exchange && var.enable_dkim && length(var.tenant_name) > 0 ? length(local.dkim) : 0}"
+  count = var.enable_exchange && var.enable_dkim && length(var.tenant_name) > 0 ? length(local.dkim) : 0
 
-    zone_id = var.zone_id
-    name    = "${lookup(local.dkim[count.index], "name")}"
-    records = ["${lookup(local.dkim[count.index], "value")}"]
-    type    = "CNAME"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = lookup(local.dkim[count.index], "name")
+  records = [lookup(local.dkim[count.index], "value")]
+  type    = "CNAME"
+  ttl     = var.ttl
 }
 
 ####################
@@ -123,13 +123,13 @@ resource "aws_route53_record" "dkim" {
 ####################
 
 resource "aws_route53_record" "sfb" {
-    count   = "${var.enable_sfb ? length(local.sfb) : 0}"
+  count = var.enable_sfb ? length(local.sfb) : 0
 
-    zone_id = var.zone_id
-    name    = "${lookup(local.sfb[count.index], "name")}"
-    records = ["${lookup(local.sfb[count.index], "record")}"]
-    type    = "${lookup(local.sfb[count.index], "type")}"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = lookup(local.sfb[count.index], "name")
+  records = [lookup(local.sfb[count.index], "record")]
+  type    = lookup(local.sfb[count.index], "type")
+  ttl     = var.ttl
 }
 
 ##########################
@@ -137,11 +137,11 @@ resource "aws_route53_record" "sfb" {
 ##########################
 
 resource "aws_route53_record" "mdm" {
-    count   = "${var.enable_mdm ? length(local.mdm) : 0}"
+  count = var.enable_mdm ? length(local.mdm) : 0
 
-    zone_id = var.zone_id
-    name    = "${lookup(local.mdm[count.index], "name")}"
-    records = ["${lookup(local.mdm[count.index], "record")}"]
-    type    = "CNAME"
-    ttl     = var.ttl
+  zone_id = var.zone_id
+  name    = lookup(local.mdm[count.index], "name")
+  records = [lookup(local.mdm[count.index], "record")]
+  type    = "CNAME"
+  ttl     = var.ttl
 }
--- variables.tf
+++ variables.tf
@@ -1,76 +1,76 @@
 variable "domain" {
-    description = "The DNS name of the domain in which to create the Office 365 records."
-    default     = ""
+  description = "The DNS name of the domain in which to create the Office 365 records."
+  default     = ""
 }
 
-variable "zone_id" { 
-    description = "The Zone ID of the Route53 DNS zone."
-    default     = ""
+variable "zone_id" {
+  description = "The Zone ID of the Route53 DNS zone."
+  default     = ""
 }
 
 variable "ttl" {
-    description = "The time-to-live value for the DNS records."
-    default     = "3600"
+  description = "The time-to-live value for the DNS records."
+  default     = "3600"
 }
 
 variable "ms_txt" {
-    description = "The value of the MS=mxXYZ TXT record which Office 365 requires to prove domain ownership."
-    default     = ""
+  description = "The value of the MS=mxXYZ TXT record which Office 365 requires to prove domain ownership."
+  default     = ""
 }
 
 variable "additional_txt_records" {
-    default     = []
-    type        = list(string)
-    description = "List of additional TXT records to add to the domain"
+  default     = []
+  type        = list(string)
+  description = "List of additional TXT records to add to the domain"
 }
 
 variable "enable_exchange" {
-    description = "Controls if the DNS records for Exchange Online should be created."
-    default     = true
-    type        = bool
+  description = "Controls if the DNS records for Exchange Online should be created."
+  default     = true
+  type        = bool
 }
 
 variable "enable_sfb" {
-    description = "Controls if the DNS records for Skype for Business should be created."
-    default     = true
-    type        = bool
+  description = "Controls if the DNS records for Skype for Business should be created."
+  default     = true
+  type        = bool
 }
 
 variable "enable_mdm" {
-    description = "Controls if the DNS records for Mobile Device Management should be created."
-    default     = true
-    type        = bool
+  description = "Controls if the DNS records for Mobile Device Management should be created."
+  default     = true
+  type        = bool
 }
 
 variable "enable_dkim" {
-    description = "Controls if the DNS records for DKIM should be created."
-    default     = false
-    type        = bool
+  description = "Controls if the DNS records for DKIM should be created."
+  default     = false
+  type        = bool
 }
 
 variable "tenant_name" {
-    description = "The Office 365 tenant name."
-    default     = ""
+  description = "The Office 365 tenant name."
+  default     = ""
 }
 
 variable "enable_dmarc" {
-    description = "Controls if the DNS record for DMARC should be created."
-    default     = false
-    type        = bool
+  description = "Controls if the DNS record for DMARC should be created."
+  default     = false
+  type        = bool
 }
 
 variable "dmarc_record" {
-    description = "The value of the DMARC record."
-    default     = ""
+  description = "The value of the DMARC record."
+  default     = ""
 }
 
 variable "enable_custom_mx" {
-    description = "Controls whether a custom MX record should be created instead of the O365 default."
-    default     = false
-    type        = bool
+  description = "Controls whether a custom MX record should be created instead of the O365 default."
+  default     = false
+  type        = bool
 }
 
 variable "custom_mx_record" {
-    description = "The value of the custom MX record to create."
-    default     = ""
+  description = "The value of the custom MX record to create."
+  default     = ""
 }
